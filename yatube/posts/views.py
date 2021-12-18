1b4970a4083bc5f9b610f5da6346861c327d449b
Sofya Serpinskaya <sofyaserpinskaya@gmail.com>
2021-12-18 22:34:11 +0300

    the last one

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -112,8 +112,8 @@ def profile_follow(request, username):
 @login_required
 def profile_unfollow(request, username):
     get_object_or_404(
-        Follow.objects.select_related('author'),
+        Follow,
         user=request.user,
-        author__in=User.objects.filter(username=username),
+        author__username=username,
     ).delete()
     return redirect('posts:profile', username=username)
